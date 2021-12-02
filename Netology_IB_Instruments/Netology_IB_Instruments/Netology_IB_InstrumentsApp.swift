d24f0aaf8d41e8d1f627ec93f9f13002d0b594ca
Evgenia Evteeva <e.n.evteeva@gmail.com>
2021-12-02 22:45:08 +0300

    5 commit

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/Netology_IB_InstrumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netology_IB_InstrumentsApp.swift
+//  Netology_IB_Instruments
+//
+//  Created by Евгения Евтеева on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Netology_IB_InstrumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift..swift
+//  Netology_IB_Instruments
+//
+//  Created by Евгения Евтеева on 30.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var dateOfBirthOfTheUser: UILabel!
+    @IBOutlet weak var userCity: UILabel!
+    @IBOutlet weak var textSignature: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
